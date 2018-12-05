c417db9bc5ee7cc1062e689d4e7417dc79a68e6d
dgrr <dgrripoll@gmail.com>
2018-12-05 12:40:37 +0100

    Added Headers data type

---
+++ headers.go
@@ -0,0 +1,171 @@
+package http2
+
+import (
+	"sync"
+)
+
+// Headers defines a FrameHeaders
+type Headers struct {
+	noCopy     noCopy
+	pad        bool
+	stream     uint32
+	weight     byte // TODO: byte or uint8?
+	hpack      *HPack
+	endStream  bool
+	endHeaders bool
+}
+
+var headersPool = sync.Pool{
+	New: func() interface{} {
+		return &Headers{}
+	},
+}
+
+// AcquireHeaders ...
+func AcquireHeaders() *Headers {
+	h := headersPool.Get().(*Headers)
+	h.hpack = AcquireHPack()
+	return h
+}
+
+// ReleaaseHeaders ...
+func ReleaseHeaders(h *Headers) {
+	h.Reset()
+	headersPool.Put(h)
+}
+
+// Reset ...
+func (h *Headers) Reset() {
+	h.pad = false
+	h.stream = 0
+	h.weight = 0
+	ReleaseHPack(h.hpack)
+	h.hpack = nil
+	h.endStream = false
+	h.endHeaders = false
+}
+
+// CopyTo copies h fields to h2.
+func (h *Headers) CopyTo(h2 *Headers) {
+	h2.pad = h.pad
+	h2.stream = h.stream
+	h2.weight = h.weight
+	if h2.hpack != nil {
+		ReleaseHPack(h2.hpack)
+	}
+	h2.hpack = h.hpack
+	h2.endStream = h.endStream
+	h2.endHeaders = h.endHeaders
+}
+
+// Add adds a name and value to the HPack header.
+func (h *Headers) Add(name, value string) {
+	h.hpack.Add(name, value)
+}
+
+// AddBytes adds a name and value to the HPack header.
+func (h *Headers) AddBytes(name, value []byte) {
+	h.hpack.AddBytes(name, value)
+}
+
+// AddBytesK ...
+func (h *Headers) AddBytesK(name []byte, value string) {
+	h.hpack.AddBytesK(name, value)
+}
+
+// AddBytesV ...
+func (h *Headers) AddBytesV(name string, value []byte) {
+	h.hpack.AddBytesV(name, value)
+}
+
+// EndStream ...
+func (h *Headers) EndStream() bool {
+	return h.endStream
+}
+
+// SetEndHeaders ...
+func (h *Headers) SetEndStream(value bool) {
+	h.endStream = value
+}
+
+// EndHeaders ...
+func (h *Headers) EndHeaders() bool {
+	return h.endHeaders
+}
+
+// SetEndHeaders ...
+func (h *Headers) SetEndHeaders(value bool) {
+	h.endHeaders = value
+}
+
+// Stream ...
+func (h *Headers) Stream() uint32 {
+	return h.stream
+}
+
+// SetStream ...
+func (h *Headers) SetStream(stream uint32) {
+	h.stream = stream
+}
+
+// Weight ...
+func (h *Headers) Weight() byte {
+	return h.weight
+}
+
+// SetWeight ...
+func (h *Headers) SetWeight(w byte) {
+	h.weight = w
+}
+
+// Padding ...
+func (h *Headers) Padding() bool {
+	return h.pad
+}
+
+// SetPadding sets padding value ...
+func (h *Headers) SetPadding(value bool) {
+	h.pad = value
+}
+
+// HPack returns the HPack of Headers.
+//
+// Do not release this field by your own.
+func (h *Headers) HPack() *HPack {
+	return h.hpack
+}
+
+// ReadFrame reads header data from fr.
+func (h *Headers) ReadFrame(fr *Frame) (err error) {
+	payload := cutPadding(fr)
+	if fr.Has(FlagPriority) {
+		h.stream = bytesToUint32(payload) & (1<<31 - 1)
+		h.weight = payload[4]
+		payload = payload[5:]
+	}
+	h.endStream = fr.Has(FlagEndStream)
+	h.endHeaders = fr.Has(FlagEndHeaders)
+	_, err = h.hpack.Read(payload)
+	return
+}
+
+// WriteToFrame ...
+func (h *Headers) WriteToFrame(fr *Frame) (err error) {
+	if h.endStream {
+		fr.Add(FlagEndStream)
+	}
+	if h.endHeaders {
+		fr.Add(FlagEndHeaders)
+	}
+	if h.pad {
+		// TODO: Write padding len
+		fr.Add(FlagPadded)
+	}
+	if h.stream > 0 && h.weight > 0 {
+		fr.Add(FlagPriority)
+		// TODO: Write stream and weight
+	}
+	fr.payload, err = h.hpack.Write(fr.payload[:0])
+	// TODO: Write padding
+	return err
+}
