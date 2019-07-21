166c70cfad0e9c2aa4ea85d76eda6f6b299defc7
Dario <dgrripoll@gmail.com>
2019-07-21 18:14:14 +0200

    Fixed TLS error and added Request in the serveConn func

--- server.go
+++ server.go
@@ -61,10 +61,13 @@ func (s *Server) Serve(ln net.Listener) error {
 		}
 
 		if cTLS, ok := c.(connTLSer); ok {
-			switch cTLS.ConnectionState().NegotiatedProtocol {
-			case H2TLSProto:
-			default:
-				err = errUpgrade
+			err = cTLS.Handshake()
+			if err == nil {
+				switch cTLS.ConnectionState().NegotiatedProtocol {
+				case H2TLSProto:
+				default:
+					err = errUpgrade
+				}
 			}
 		}
 
@@ -135,6 +138,7 @@ func (s *Server) serveConn(c net.Conn) (err error) {
 
 	for err == nil {
 		fr.Reset()
+		shouldHandle := false
 		cLastStreamID = cStreamID
 
 		_, err = fr.ReadFrom(c) // TODO: Use ReadFromLimitPayload?
@@ -171,44 +175,43 @@ func (s *Server) serveConn(c net.Conn) (err error) {
 			streams[fr.Stream()] = ctx
 		}
 
-		switch fr.Type() {
-		case FrameData:
-			println("data")
-			ctx.Request.body.Write(
-				peekDataPayload(fr.Payload()),
-			)
-		case FrameHeaders:
-			println("headers")
-			ctx.Request.Header.rawHeaders = append(
-				ctx.Request.Header.rawHeaders, peekHeadersPayload(fr)...,
-			)
-			// headers will be parsed in the handler
-			if shouldServe = fr.Has(FlagEndHeaders) && fr.Has(FlagEndStream); shouldServe {
-				ctx.Request.Header.parseRawHeaders()
+		err = ctx.Request.ReadFrom(fr)
+		switch err {
+		case nil:
+			shouldHandle = ctx.Request.Header.IsGet() || ctx.Request.Header.IsHead()
+		case errCannotHandle:
+			switch fr.Type() {
+			case FramePriority:
+				println("priority")
+			case FrameResetStream:
+				println("reset")
+			case FrameSettings:
+				println("settings")
+				// TODO: Check if the client's settings fit the server ones
+				// reading settings frame
+				err = userSettings.ReadFrame(fr)
+				if err == nil {
+					userSettings.ack = true
+					fr := AcquireFrame()
+					userSettings.WriteFrame(fr)
+					err = ctx.writeFrame(fr)
+					ReleaseFrame(fr)
+				}
+			case FramePushPromise:
+				println("pp")
+			case FramePing:
+				println("ping")
+			case FrameGoAway:
+				println("away")
+			case FrameWindowUpdate:
+				println("update")
+			case FrameContinuation:
+				println("continuation")
 			}
-		case FramePriority:
-			println("priority")
-		case FrameResetStream:
-			println("reset")
-		case FrameSettings:
-			println("settings")
-			// TODO: Check if the client's settings fit the server ones
-			// reading settings frame
-			err = userSettings.ReadFrame(fr)
-			if err == nil {
-				userSettings.ack = true
-				err = writeSettings(c, sStreamID, &userSettings)
-			}
-		case FramePushPromise:
-			println("pp")
-		case FramePing:
-			println("ping")
-		case FrameGoAway:
-			println("away")
-		case FrameWindowUpdate:
-			println("update")
-		case FrameContinuation:
-			println("continuation")
+		}
+
+		if shouldHandle {
+			s.Handler(ctx)
 		}
 	}
 
