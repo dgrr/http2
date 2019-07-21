8cb565f781eaff82294e5f734dbb6cd8657677d7
Dario <dgrripoll@gmail.com>
2019-07-21 16:43:45 +0200

    Added Ctx structure

--- ctx.go
+++ ctx.go
@@ -1,5 +1,22 @@
 package http2
 
-type Ctx struct {
+import (
+	"net"
+)
 
+type Ctx struct {
+	c        net.Conn
+	streamID uint32
+	hp       *HPACK
+
+	Request  Request
+	Response Response
+}
+
+func (ctx *Ctx) SetHPACK(hp *HPACK) {
+	ctx.hp = hp
+}
+
+func (ctx *Ctx) SetStream(sid uint32) {
+	ctx.streamID = sid
 }
