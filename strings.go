a59f06c440639b78e5e390aa199ea54a4eb2dc5f
Dario <dgrripoll@gmail.com>
2019-07-21 18:13:31 +0200

    Added strings.go

---
+++ strings.go
@@ -0,0 +1,9 @@
+package http2
+
+var (
+	strUserAgent = []byte("user-agent")
+	strPath      = []byte("path")
+	strMethod    = []byte("method")
+	strGET       = []byte("GET")
+	strHEAD      = []byte("HEAD")
+)
