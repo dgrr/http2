83565196dfd43a304cda0cbe02c993666c3f6d34
Dario <dgrripoll@gmail.com>
2019-07-21 21:36:14 +0200

    Added parse method in response

--- response.go
+++ response.go
@@ -1,6 +1,7 @@
 package http2
 
 import (
+	"strconv"
 	"sync"
 
 	"github.com/valyala/bytebufferpool"
@@ -15,7 +16,7 @@ var resPool = sync.Pool{
 type Response struct {
 	Header ResponseHeader
 
-	b *bytebufferpool.ByteBuffer
+	b bytebufferpool.ByteBuffer
 }
 
 // AcquireResponse ...
@@ -28,23 +29,33 @@ func (res *Response) Reset() {
 	res.b.Reset()
 }
 
+func (res *Response) Write(b []byte) (int, error) {
+	n, _ := res.b.Write(b)
+	res.Header.contentLength += n
+	return n, nil
+}
+
+func (res *Response) Body() []byte {
+	return res.b.Bytes()
+}
+
 // ResponseHeader ...
 type ResponseHeader struct {
-	hs []*HeaderField
-	hp *HPACK
+	hs            []*HeaderField
+	hp            *HPACK
+	raw           []byte
+	contentLength int
+	statusCode    int
 }
 
 func (h *ResponseHeader) Reset() {
 	h.hs = h.hs[:0]
+	h.raw = h.raw[:0]
+	h.statusCode = 0
 }
 
 // Get ...
-func (h *ResponseHeader) Get(key []byte) *HeaderField {
-	return h.GetString(b2s(key))
-}
-
-// GetString ...
-func (h *ResponseHeader) GetString(key string) (hf *HeaderField) {
+func (h *ResponseHeader) Get(key string) (hf *HeaderField) {
 	for i := range h.hs {
 		if b2s(h.hs[i].key) == key {
 			hf = h.hs[i]
@@ -53,4 +64,37 @@ func (h *ResponseHeader) GetString(key string) (hf *HeaderField) {
 	}
 
 	return
+}
+
+// GetString ...
+func (h *ResponseHeader) GetBytes(key []byte) *HeaderField {
+	return h.Get(b2s(key))
+}
+
+func (h *ResponseHeader) SetStatusCode(code int) {
+	h.statusCode = code
+}
+
+func (h *ResponseHeader) Add(k, v string) {
+	hf := AcquireHeaderField()
+	hf.Set(k, v)
+	h.hs = append(h.hs, hf)
+}
+
+func (h *ResponseHeader) parse() {
+	hf := AcquireHeaderField()
+	defer ReleaseHeaderField(hf)
+
+	if h.statusCode <= 0 {
+		h.statusCode = 200
+	}
+	hf.SetKey(":status")
+	hf.value = strconv.AppendInt(hf.value[:0], int64(h.statusCode), 10)
+	h.raw = h.hp.AppendHeader(hf, h.raw[:0])
+
+	if h.contentLength > 0 {
+		hf.SetKey("content-length")
+		hf.value = strconv.AppendInt(hf.value[:0], int64(h.contentLength), 10)
+		h.raw = h.hp.AppendHeader(hf, h.raw)
+	}
 }
