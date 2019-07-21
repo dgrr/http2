4b5dded7a864c8da5ef76e8a882f1a9696bac56f
Dario <dgrripoll@gmail.com>
2019-07-21 18:15:33 +0200

    Added Request.parse()

--- request.go
+++ request.go
@@ -1,6 +1,8 @@
 package http2
 
 import (
+	"bytes"
+	"errors"
 	"sync"
 
 	"github.com/valyala/bytebufferpool"
@@ -13,8 +15,10 @@ var reqPool = sync.Pool{
 }
 
 type Request struct {
-	h RequestHeaders
-	b *bytebufferpool.ByteBuffer
+	Header RequestHeader
+
+	b        *bytebufferpool.ByteBuffer
+	lastType uint8 // last type of frame
 }
 
 // AcquireRequest ...
@@ -23,33 +27,157 @@ func AcquireRequest() *Request {
 }
 
 func (req *Request) Reset() {
-	req.h.Reset()
+	req.Header.Reset()
 	req.b.Reset()
 }
 
-// RequestHeaders ...
-type RequestHeaders struct {
-	h []*HeaderField
+var (
+	errCannotHandle      = errors.New("cannot handle this frame type")
+	errLastTypeDontMatch = errors.New("last type doesn't match any")
+)
+
+func (req *Request) ReadFrom(fr *Frame) error {
+	var err error
+
+	switch fr.Type() {
+	case FrameContinuation:
+		switch req.lastType {
+		case FrameData:
+			fr.SetType(FrameData)
+			err = req.ReadFrom(fr)
+		case FrameHeaders:
+			fr.SetType(FrameHeaders)
+			err = req.ReadFrom(fr)
+		default:
+			err = errLastTypeDontMatch
+		}
+	case FrameData:
+		data := AcquireData()
+		err = data.ReadFrame(fr)
+		if err == nil {
+			req.b.Write(data.b)
+		}
+		ReleaseData(data)
+	case FrameHeaders:
+		h := AcquireHeaders()
+		err = h.ReadFrame(fr)
+		if err == nil {
+			req.Header.Write(h.rawHeaders)
+			if fr.Has(FlagEndHeaders) {
+				req.Header.parse()
+			}
+		}
+		ReleaseHeaders(h)
+	default:
+		err = errCannotHandle
+	}
+
+	if err == nil {
+		req.lastType = fr.Type()
+	}
+
+	return err
 }
 
-func (h *RequestHeaders) Reset() {
+// RequestHeader ...
+type RequestHeader struct {
+	path      []byte
+	method    []byte
+	userAgent []byte
+
+	h   []*HeaderField
+	hp  *HPACK
+	raw []byte
+}
+
+func (h *RequestHeader) IsGet() bool {
+	return bytes.Equal(h.method, strGET)
+}
+
+func (h *RequestHeader) IsHead() bool {
+	return bytes.Equal(h.method, strHEAD)
+}
+
+func (h *RequestHeader) Path() []byte {
+	return h.path
+}
+
+func (h *RequestHeader) Method() []byte {
+	return h.method
+}
+
+func (h *RequestHeader) UserAgent() []byte {
+	return h.userAgent
+}
+
+func (h *RequestHeader) Reset() {
 	// TODO: free resources
 	h.h = h.h[:0]
+	h.raw = h.raw[:0]
+}
+
+func (h *RequestHeader) parse() (err error) {
+	hp := h.hp
+	hf := AcquireHeaderField()
+
+	for len(h.raw) > 0 {
+		h.raw, err = hp.Next(hf, h.raw)
+		if err != nil {
+			break
+		}
+		if len(hf.key) > 0 {
+			switch hf.key[0] | 0x20 {
+			case 'u':
+				if equalsFold(hf.key, strUserAgent) {
+					h.userAgent = append(h.userAgent[:0], hf.value...)
+					continue
+				}
+			case ':':
+				hf.key = append(hf.key[:0], hf.key[1:]...)
+				if len(hf.key) == 0 {
+					continue
+				}
+
+				switch hf.key[0] {
+				case 'p':
+					if equalsFold(hf.key, strPath) {
+						h.path = append(h.path[:0], hf.value...)
+						continue
+					}
+				case 'm':
+					if equalsFold(hf.key, strMethod) {
+						h.method = append(h.method[:0], hf.value...)
+						continue
+					}
+				}
+			}
+			h.h = append(h.h, hf)
+			hf = AcquireHeaderField()
+		}
+	}
+
+	ReleaseHeaderField(hf)
+
+	return err
+}
+
+func (h *RequestHeader) Write(b []byte) (int, error) {
+	h.raw = append(h.raw, b...)
+	return len(b), nil
 }
 
 // Get ...
-func (h *RequestHeaders) Get(key []byte) *HeaderField {
+func (h *RequestHeader) Get(key []byte) *HeaderField {
 	return h.GetString(b2s(key))
 }
 
 // GetString ...
-func (h *RequestHeaders) GetString(key string) (hf *HeaderField) {
+func (h *RequestHeader) GetString(key string) (hf *HeaderField) {
 	for i := range h.h {
 		if b2s(h.h[i].key) == key {
 			hf = h.h[i]
 			break
 		}
 	}
-
 	return
 }
