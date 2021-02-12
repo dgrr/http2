455f75ab35c2d9979f94951cdc2f9c21e23c8917
Dario <dgrripoll@gmail.com>
2021-02-12 18:48:52 +0100

    gitignore. Ideal example

---
+++ examples/simple.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+	"github.com/dgrr/http2"
+)
+
+func main() {
+	cert, priv, err = fasthttp.GenerateTestCertificate("localhost:8080")
+        if err != nil {
+                log.Fatalln(err)
+        }
+
+	s := &fasthttp.Server{
+		Handler: requestHandler,
+		Name: "http2 test",
+	}
+	err := s.AppendCertEmbed(cert, priv)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	http2.ConfigureServer(s)
+
+	err := s.ListenAndServeTLS(":8443", "", "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hello world!\n")
+}
