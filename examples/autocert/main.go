f4f7a8e12f004d6cbc3afe7b561656cc00335e02
Dario <dgrripoll@gmail.com>
2021-02-16 17:11:05 +0100

    Added autocert example #14

---
+++ examples/autocert/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"time"
+	"log"
+        "crypto/tls"
+	"encoding/pem"
+        "fmt"
+        "net/http"
+
+        "golang.org/x/crypto/acme/autocert"
+        "golang.org/x/crypto/acme"
+        "github.com/dgrr/http2"
+        "github.com/valyala/fasthttp"
+)
+
+
+func main() {
+	hostName := "example.com"
+
+	cert, priv, err := configureCert(hostName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+        s := &fasthttp.Server{
+                Handler: requestHandler,
+                Name:    "http2 test",
+        }
+        http2.ConfigureServer(s)
+
+	log.Println("fasthttp", s.ListenAndServeTLSEmbed(":443", cert, priv))
+}
+
+func configureCert(hostName string) ([]byte, []byte, error) {
+	m := &autocert.Manager{
+                Prompt:     autocert.AcceptTOS,
+                HostPolicy: autocert.HostWhitelist(hostName),
+                Cache:      autocert.DirCache("./certs"),
+        }
+
+        cfg := &tls.Config{
+                GetCertificate: m.GetCertificate,
+                NextProtos: []string{
+                        acme.ALPNProto,
+                },
+        }
+
+	s := &http.Server{
+		Addr: ":80",
+		Handler: m.HTTPHandler(nil),
+		TLSConfig: cfg,
+	}
+	go s.ListenAndServe()
+
+	time.Sleep(time.Second * 10)
+	s.Shutdown(context.Background())
+
+	data, err := m.Cache.Get(context.Background(), hostName)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	priv, restBytes := pem.Decode(data)
+	cert, _ := pem.Decode(restBytes)
+
+	return pem.EncodeToMemory(cert), pem.EncodeToMemory(priv), nil
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+        fmt.Printf("%s\n", ctx.Request.Header.Header())
+        if ctx.Request.Header.IsPost() {
+                fmt.Fprintf(ctx, "%s\n", ctx.Request.Body())
+        } else {
+                fmt.Fprintf(ctx, "Hello 21th century!\n")
+        }
+}
