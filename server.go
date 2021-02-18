19504d4fb9c507c10eb02b9fe670a6c16895eec2
Artūrs Mekšs <arturs.mekss@gmail.com>
2021-02-18 20:19:36 +0200

    Fix: execute handlers if EndStream flag is received in Headers
    According to the docs https://httpwg.org/specs/rfc7540.html#n-examples END_STREAM flag can be sent in headers if DATA will not follow.

--- server.go
+++ server.go
@@ -365,10 +365,9 @@ func (s *Server) parseHeaders(ctx *connCtx, strm *Stream, isEnd bool) (err error
 	}
 
 	if err == nil {
-		if strm.ctx.Request.Header.IsGet() ||
-			strm.ctx.Request.Header.IsHead() {
+		if strm.hfr.EndStream() {
 			strm.istate = stateExecHandler
-		} else { // post, put or delete
+		} else {
 			strm.istate = stateAwaitData
 		}
 		strm.ctx.Request.SetRequestURIBytes(
